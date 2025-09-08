fbfedda352a338fc5525118865ed7bbe33b953d7
Ricardo Montañez Miranda <medinova.cuu@gmail.com>
2025-09-08 11:54:18 -0600

    Initial Commit

---
+++ XibAppTests/XibAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XibAppTests.swift
+//  XibAppTests
+//
+//  Created by Ricardo Montañez Miranda on 08/09/25.
+//
+
+import Testing
+@testable import XibApp
+
+struct XibAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
